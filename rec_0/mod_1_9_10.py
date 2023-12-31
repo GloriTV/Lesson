fd00d3a315a98be8c711a9df02f91daa66c3c2f0
Glorit <glorit@yandex.ru>
2024-01-01 00:56:34 +0500

    01.01.24

--- rec_0/mod_1_9_10.py
+++ rec_0/mod_1_9_10.py
@@ -18,20 +18,43 @@ def shuffle_seed(array):
 # print(shuffle_seed(array))
 
 def min_max_dist(*vectors):
-    max_np=0
-    min_np=10
+    min_max=list()
     x_el=len(vectors)
     
     for x in range(x_el-1):
         for y in range(x+1,x_el):
-            var=np.linalg.norm(vectors[x] - vectors[y])
-            if var<min_np: min_np=var
-            if var>max_np: max_np=var
-    return (min_np,max_np)
+            min_max.append(np.linalg.norm(vectors[x] - vectors[y]))
+            
+    return (min(min_max),max(min_max))
 
-vec1 = np.array([1,2,3])
-vec2 = np.array([4,5,6])
-vec3 = np.array([7, 8, 9])
+# vec1 = np.array([1,2,3])
+# vec2 = np.array([4,5,6])
+# vec3 = np.array([7, 8, 9])
 
-print(min_max_dist(vec1, vec2, vec3))
+# print(min_max_dist(vec1, vec2, vec3))
 # (5.196152422706632, 10.392304845413264)
+
+def any_normal(*vectors): 
+    x_el=len(vectors)
+    
+    for x in range(x_el-1):
+        for y in range(x+1,x_el):
+            if np.dot(vectors[x] , vectors[y]) == 0 :return True
+         
+    return False
+
+# vec1 = np.array([2, 1])
+# vec2 = np.array([-1, 2])
+# vec3 = np.array([3,4])
+# print(any_normal(vec1, vec2, vec3))
+# True
+
+def get_loto(num):
+    result=np.zeros((num,5,5))
+    workers=range(1,101)
+    for x in range(num):
+        result[x]=np.random.choice(workers, size=(5,5), replace=False)
+        
+    return result
+
+# print(get_loto(3))
